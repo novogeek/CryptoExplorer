e3a276419277c5d6efcb84da3c31828da83e564d
Krishna Chaitanya T <krishnachaitanya.t@live.com>
2017-04-24 23:06:03 +0530

    First commit
    Crypto Explorer - A simple utility to graphically demonstrate the
    weaknesses in poor Cryptography implementations. Specifically, it
    demonstrates the weakness in Stream Cipher key stream reuse, and Block
    cipher ECB mode

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
+using Drawing = System.Drawing;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace CryptoExplorer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string _blockFilePath = String.Empty;
+        private string _streamSourceFilePath = String.Empty;
+        private string _streamKeyfilePath = String.Empty;
+        private Drawing.Bitmap _encryptedBitmap;
+        public MainWindow()
+        {
+            InitializeComponent();
+            EnableControls(false);
+        }
+        private void btnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.png;*.jpg;*.jpeg)|*.png;*.jpg;*.jpeg";
+            openFileDialog.InitialDirectory = System.AppDomain.CurrentDomain.BaseDirectory;
+
+            Nullable<bool> result = openFileDialog.ShowDialog();
+            if (result == true) {
+                btnEncrypt.IsEnabled = true;
+                _blockFilePath = openFileDialog.FileName; 
+                txtFileName.Text = _blockFilePath;
+                SetWpfImageFromPath(_blockFilePath, imgDisplay);
+            }
+        }        
+        private void btnEncrypt_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                CipherMode mode= CipherMode.CBC;
+                if ((bool)rbEncryptionModeCBC.IsChecked) { mode = CipherMode.CBC; }
+                else if ((bool)rbEncryptionModeECB.IsChecked) { mode = CipherMode.ECB; }
+                _encryptedBitmap = CryptoHelper.EncryptImage(_blockFilePath, mode);
+                SetWpfImageFromImage(_encryptedBitmap, imgDisplay);
+                btnDecrypt.IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        private void btnDecrypt_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                CipherMode mode = CipherMode.CBC;
+                if ((bool)rbEncryptionModeCBC.IsChecked) { mode = CipherMode.CBC; }
+                else if ((bool)rbEncryptionModeECB.IsChecked) { mode = CipherMode.ECB; }
+                Drawing.Image decryptedImg = CryptoHelper.DecryptImage(_encryptedBitmap, mode);
+                SetWpfImageFromImage(decryptedImg, imgDisplay);
+                btnDecrypt.IsEnabled = false;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        private void SetWpfImageFromPath(string filePath, System.Windows.Controls.Image imgControl)
+        {
+            BitmapImage bitmapImg = new BitmapImage();
+            bitmapImg.BeginInit();
+            bitmapImg.UriSource = new Uri(filePath, UriKind.Absolute);
+            bitmapImg.EndInit();
+            imgControl.Source = bitmapImg;
+        }
+        private void SetWpfImageFromImage(Drawing.Image img, System.Windows.Controls.Image imgControl) {
+            using (var ms = new MemoryStream())
+            {
+                img.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                BitmapImage bitmapImg = new BitmapImage();
+                bitmapImg.BeginInit();
+                bitmapImg.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImg.StreamSource = ms;
+                bitmapImg.EndInit();
+                imgControl.Source = bitmapImg;
+            }
+        }
+        private void EnableControls(bool isEnabled)
+        {
+            btnEncrypt.IsEnabled = isEnabled;
+            btnDecrypt.IsEnabled = isEnabled;
+            btnEncryptDecryptStream.IsEnabled = isEnabled;
+        }
+
+        private void btnBrowseSourcePic_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.png;*.jpg;*.jpeg)|*.png;*.jpg;*.jpeg";
+            openFileDialog.InitialDirectory = System.AppDomain.CurrentDomain.BaseDirectory;
+
+            Nullable<bool> result = openFileDialog.ShowDialog();
+            if (result == true)
+            {
+                //btnEncryptStream.IsEnabled = true;
+                _streamSourceFilePath = openFileDialog.FileName;
+                txtFileNameSourcePic.Text = _streamSourceFilePath;
+                //SetWpfImageFromPath(_filePath, imgDisplayStream);
+            }
+        }
+
+        private void btnBrowseKeyStream_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.png;*.jpg;*.jpeg)|*.png;*.jpg;*.jpeg";
+            openFileDialog.InitialDirectory = System.AppDomain.CurrentDomain.BaseDirectory;
+
+            Nullable<bool> result = openFileDialog.ShowDialog();
+            if (result == true)
+            {
+                btnEncryptDecryptStream.IsEnabled = true;
+                _streamKeyfilePath = openFileDialog.FileName;
+                txtFileNameKeyStream.Text = _streamKeyfilePath;
+                //SetWpfImageFromPath(_filePath, imgDisplayStream);
+            }
+        }
+        private void btnEncryptDecryptStream_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                _encryptedBitmap = XorHelper.EncryptImage(_streamSourceFilePath, _streamKeyfilePath);
+                SetWpfImageFromImage(_encryptedBitmap, imgDisplayStream);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        private void btnSaveStream_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Image files (*.png;*.jpg;*.jpeg)|*.png;*.jpg;*.jpeg";
+            saveFileDialog.InitialDirectory = System.AppDomain.CurrentDomain.BaseDirectory;
+
+            if (saveFileDialog.ShowDialog() == true)
+                File.WriteAllBytes(saveFileDialog.FileName, CryptoHelper.imageToByteArray(_encryptedBitmap));
+        }
+    }
+}
---
+++ XorHelper.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CryptoExplorer
+{
+    public static class XorHelper
+    {
+        private static byte[] _encryptedBytes = null;
+        private static int _bytes;
+        private static byte[] _iv = DeriveKey();
+        public static Bitmap EncryptImage(this string filePath, string keyStreamFilePath)
+        {
+            try
+            {
+                byte[] iv=GetKeyStreamBytes(keyStreamFilePath);
+                Bitmap bmp = new Bitmap(Bitmap.FromFile(filePath)); // Pick image from file path
+
+                // Lock the bitmap's bits.  
+                Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+                System.Drawing.Imaging.BitmapData bmpData =
+                    bmp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
+                    bmp.PixelFormat);
+
+                // Get the address of the first line.
+                IntPtr ptr = bmpData.Scan0;
+
+                // Declare an array to hold the bytes of the bitmap.
+                int bytes = Math.Abs(bmpData.Stride) * bmp.Height;
+                byte[] contentBytes = new byte[bytes];
+
+                // Copy the content bytes into the array (This does not copy the image header!).
+                System.Runtime.InteropServices.Marshal.Copy(ptr, contentBytes, 0, bytes);
+
+                byte[] modifiedBytes = new byte[contentBytes.Length];
+                // Modify the bytes
+
+                //for (int i = 0; i < contentBytes.Length; i++) {
+                //    modifiedBytes[i] = (byte) (contentBytes[i] ^ _iv[i]);
+                //}
+                modifiedBytes = Xor(contentBytes, iv);
+
+                _encryptedBytes = modifiedBytes; // Store the encrypted bytes in memory, for later use in decryption routine.
+                _bytes = bytes;
+
+                // Copy the modified values back to the bitmap
+                System.Runtime.InteropServices.Marshal.Copy(modifiedBytes, 0, ptr, bytes);
+
+                // Unlock the bits.
+                bmp.UnlockBits(bmpData);
+                return bmp;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private static byte[] GetKeyStreamBytes(string keyStreamFilePath) {
+            Bitmap bmp = new Bitmap(Bitmap.FromFile(keyStreamFilePath)); // Pick image from file path
+
+            // Lock the bitmap's bits.  
+            Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            System.Drawing.Imaging.BitmapData bmpData =
+                bmp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
+                bmp.PixelFormat);
+
+            // Get the address of the first line.
+            IntPtr ptr = bmpData.Scan0;
+
+            // Declare an array to hold the bytes of the bitmap.
+            int bytes = Math.Abs(bmpData.Stride) * bmp.Height;
+            byte[] contentBytes = new byte[bytes];
+
+            // Copy the content bytes into the array (This does not copy the image header!).
+            System.Runtime.InteropServices.Marshal.Copy(ptr, contentBytes, 0, bytes);
+            return contentBytes;
+        }
+        private static byte[] DeriveKey()
+        {
+            using (RandomNumberGenerator rng = new RNGCryptoServiceProvider())
+            {
+                byte[] tokenData = new byte[3200000];
+                rng.GetBytes(tokenData);
+                return tokenData;
+            }
+        }
+        //Performs byte-wise XOR between elements of @a and @b
+        public static byte[] Xor(byte[] a, byte[] b)
+        {
+            if (a == null || b == null)
+                return null;
+
+            int len = Math.Max(a.Length, b.Length);
+
+            byte[] res = new byte[len];
+
+            if (a.Length > b.Length)
+            {
+                a.CopyTo(res, 0);
+                for (int i = 0; i < b.Length; i++)
+                {
+                    res[i] ^= b[i];
+                }
+            }
+            else
+            {
+                b.CopyTo(res, 0);
+                for (int i = 0; i < a.Length; i++)
+                {
+                    res[i] ^= a[i];
+                }
+            }
+
+            return res;
+        }
+    }
+}
